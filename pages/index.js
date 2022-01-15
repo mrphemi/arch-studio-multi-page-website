7e611cb975e91835a0b36b9fb4fb0814a50d150d
Oluwafemi Kayode <mrlapinho@gmail.com>
2022-01-15 17:50:33 +0000

    Add fonts and colors

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <div>
-      <h1>ARCH STUDIO</h1>
+      <h1>spartan bold</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        ex architecto quas odio pariatur reprehenderit ea, eaque, minima, id
+        repellendus aspernatur nulla magni perferendis sequi tempora enim
+        reiciendis quam eum. Culpa deleniti temporibus facere in recusandae
+        fuga. Nihil veniam non, quae quidem maxime, tenetur blanditiis dolorum
+        architecto assumenda, placeat officia.
+      </p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Spartan", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "dark-grey": "#60636D",
+        "medium-grey": "#7D828F",
+        "light-grey": "#C8CCD8",
+        "very-light-grey": "#EEEFF4",
+        red: "#DF5656",
+        "very-dark-blue": "#1B1D23",
+      },
+      fontSize: {
+        base: "15px",
+        40: "40px",
+        56: "56px",
+        80: "80px",
+        120: "120px",
+        largest: "200px",
+      },
+      lineHeight: {
+        25: "25px",
+        48: "48px",
+        56: "56px",
+        80: "80px",
+        large: "200px",
+      },
+    },
   },
   plugins: [],
 };
